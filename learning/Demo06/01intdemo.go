8b557be69d9fb43e5ddfc2054893fd2307a32483
FKBGRENSHE <fkbgrenshe@163.com>
2026-04-28 18:46:56 +0800

    new files

--- learning/Demo06/01intdemo.go
+++ learning/Demo06/01intdemo.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func main() {
+	var num int = 10
+	fmt.Printf("num=%v type=%T \n", num, num)
+
+	s1 := `first line
+second line
+third line
+\n
+`
+
+	s2 := `other line`
+
+	var flag = strings.Contains(s1, s2)
+	fmt.Println(flag)
+	var str = "123-456-789"
+	var arr = strings.Split(str, "-")
+	var str2 = strings.Join(arr, "*")
+	fmt.Println(str2)
 }
